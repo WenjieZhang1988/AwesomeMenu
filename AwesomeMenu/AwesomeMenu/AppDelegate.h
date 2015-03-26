52c1d819cd7a92bcfdc56f5877ce2716693f2c68
Kevin <Kevin.1988@foxmail.com>
2015-03-27 00:34:35 +0800

    项目初始化

---
+++ AwesomeMenu/AwesomeMenu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AwesomeMenu
+//
+//  Created by Kevin on 15/3/27.
+//  Copyright (c) 2015年 Kevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
